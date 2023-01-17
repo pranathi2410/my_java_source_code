1cde9512c4fca9b4a44b23bb75b0c40f0b5ae491
pranathi2410 <122851035+pranathi2410@users.noreply.github.com>
2023-01-17 12:08:16 +0530

    Add files via upload

---
+++ array1.java
@@ -0,0 +1,18 @@
+package mypackage;
+
+public class array1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr={12,34,14,33,22,11,19,45,61};
+		{
+			for(int i=arr.length-1;i>=0;i--)
+			{
+			System.out.println(arr[i]);	
+			}
+		}
+		
+
+	}
+
+}
